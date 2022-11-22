bc00b931fc99866a2f4d543e9a657138f5c71d2a
KieuAnh <anhhkph24928@fpt.edu.vn>
2022-11-22 15:12:54 +0700

    OKEOK

---
+++ 3.PL/Views/FrmCodeQBR.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace _3.PL.Views
+{
+    partial class FrmCodeQBR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_barcode = new System.Windows.Forms.Button();
+            this.txt_barcode = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_qrcode = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(41, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Barcode";
+            // 
+            // btn_barcode
+            // 
+            this.btn_barcode.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_barcode.Location = new System.Drawing.Point(499, 35);
+            this.btn_barcode.Name = "btn_barcode";
+            this.btn_barcode.Size = new System.Drawing.Size(94, 29);
+            this.btn_barcode.TabIndex = 1;
+            this.btn_barcode.Text = "Generate";
+            this.btn_barcode.UseVisualStyleBackColor = true;
+            this.btn_barcode.Click += new System.EventHandler(this.btn_barcode_Click);
+            // 
+            // txt_barcode
+            // 
+            this.txt_barcode.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt_barcode.Location = new System.Drawing.Point(140, 36);
+            this.txt_barcode.Name = "txt_barcode";
+            this.txt_barcode.Size = new System.Drawing.Size(282, 31);
+            this.txt_barcode.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(140, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btn_qrcode
+            // 
+            this.btn_qrcode.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_qrcode.Location = new System.Drawing.Point(499, 104);
+            this.btn_qrcode.Name = "btn_qrcode";
+            this.btn_qrcode.Size = new System.Drawing.Size(94, 29);
+            this.btn_qrcode.TabIndex = 4;
+            this.btn_qrcode.Text = "Generate";
+            this.btn_qrcode.UseVisualStyleBackColor = true;
+            this.btn_qrcode.Click += new System.EventHandler(this.btn_qrcode_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(41, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "QRCode";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(124, 167);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 193);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_save.Location = new System.Drawing.Point(299, 393);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(94, 29);
+            this.btn_save.TabIndex = 7;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // FrmCodeQBR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_barcode);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_qrcode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_barcode);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCodeQBR";
+            this.Text = "FrmCodeQBR";
+            this.Load += new System.EventHandler(this.FrmCodeQBR_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_barcode;
+        private System.Windows.Forms.TextBox txt_barcode;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_qrcode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_save;
+    }
+}
---
+++ 3.PL/Views/FrmCodeQBR.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.PL.Views
+{
+    public partial class FrmCodeQBR : Form
+    { 
+        bool generrated = false;
+        public FrmCodeQBR()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCodeQBR_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_barcode_Click(object sender, EventArgs e)
+        {
+            generrated = true;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            Zen.Barcode.Code128BarcodeDraw barcode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            pictureBox1.Image = barcode.Draw(btn_barcode.Text, 200);
+        }
+
+        private void btn_qrcode_Click(object sender, EventArgs e)
+        {
+            generrated = true;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            Zen.Barcode.CodeQrBarcodeDraw qrcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            pictureBox1.Image = qrcode.Draw(btn_barcode.Text, 200);
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            if (generrated)
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                pictureBox1.Image.Save(path + "\\" + DateTime.Now.Second.ToString() + DateTime.Now.Millisecond.ToString() + ".jpg",ImageFormat.Jpeg);
+            }
+        }
+    }
+}
