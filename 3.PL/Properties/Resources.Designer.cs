e9596d8f8ee45784163f5b860c0fce7833e13ad3
Kaisan03 <khainqph24697@fpt.edu.vn>
2022-11-23 16:19:04 +0700

    anh

--- 3.PL/Properties/Resources.Designer.cs
+++ 3.PL/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace _3.PL.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -166,6 +166,26 @@ namespace _3.PL.Properties {
         internal static System.Drawing.Bitmap icons8_sale_32 {
             get {
                 object obj = ResourceManager.GetObject("icons8-sale-32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_sharingan_32 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-sharingan-32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_sharingan_321 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-sharingan-321", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
