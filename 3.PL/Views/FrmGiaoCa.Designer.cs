4baf7ba51c2e28f586237ce6058a1e56125dd3b5
Hoành <101155961+HoanhDH@users.noreply.github.com>
2022-12-04 16:50:25 +0700

    OKI

---
+++ 3.PL/Views/FrmGiaoCa.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace _3.PL.Views
+{
+    partial class FrmGiaoCa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_maca = new System.Windows.Forms.Label();
+            this.lb_thoigianbatdau = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_thoigianketthuc = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lb_tienbandau = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lb_tienmat = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lb_tienchuyenkhoan = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.lb_tongdoanhthutrongca = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this._tongtienmat = new System.Windows.Forms.Label();
+            this.lb_tongdoanhthu = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.tbx_tienphatsinh = new System.Windows.Forms.TextBox();
+            this.tbx_ghichu = new System.Windows.Forms.RichTextBox();
+            this.btn_resettien = new FontAwesome.Sharp.IconButton();
+            this.btn_xacnhan = new FontAwesome.Sharp.IconButton();
+            this.btn_huy = new FontAwesome.Sharp.IconButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Ca :";
+            // 
+            // lb_maca
+            // 
+            this.lb_maca.AutoSize = true;
+            this.lb_maca.Location = new System.Drawing.Point(226, 27);
+            this.lb_maca.Name = "lb_maca";
+            this.lb_maca.Size = new System.Drawing.Size(38, 15);
+            this.lb_maca.TabIndex = 1;
+            this.lb_maca.Text = "label2";
+            // 
+            // lb_thoigianbatdau
+            // 
+            this.lb_thoigianbatdau.AutoSize = true;
+            this.lb_thoigianbatdau.Location = new System.Drawing.Point(226, 67);
+            this.lb_thoigianbatdau.Name = "lb_thoigianbatdau";
+            this.lb_thoigianbatdau.Size = new System.Drawing.Size(38, 15);
+            this.lb_thoigianbatdau.TabIndex = 3;
+            this.lb_thoigianbatdau.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Thời gian bắt đầu :";
+            // 
+            // lb_thoigianketthuc
+            // 
+            this.lb_thoigianketthuc.AutoSize = true;
+            this.lb_thoigianketthuc.Location = new System.Drawing.Point(226, 107);
+            this.lb_thoigianketthuc.Name = "lb_thoigianketthuc";
+            this.lb_thoigianketthuc.Size = new System.Drawing.Size(38, 15);
+            this.lb_thoigianketthuc.TabIndex = 5;
+            this.lb_thoigianketthuc.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Thời gian kết thúc :";
+            // 
+            // lb_tienbandau
+            // 
+            this.lb_tienbandau.AutoSize = true;
+            this.lb_tienbandau.Location = new System.Drawing.Point(226, 147);
+            this.lb_tienbandau.Name = "lb_tienbandau";
+            this.lb_tienbandau.Size = new System.Drawing.Size(38, 15);
+            this.lb_tienbandau.TabIndex = 7;
+            this.lb_tienbandau.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(31, 147);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 15);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Tiền ban đầu :";
+            // 
+            // lb_tienmat
+            // 
+            this.lb_tienmat.AutoSize = true;
+            this.lb_tienmat.Location = new System.Drawing.Point(226, 267);
+            this.lb_tienmat.Name = "lb_tienmat";
+            this.lb_tienmat.Size = new System.Drawing.Size(44, 15);
+            this.lb_tienmat.TabIndex = 13;
+            this.lb_tienmat.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(31, 267);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(59, 15);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Tiền mặt :";
+            // 
+            // lb_tienchuyenkhoan
+            // 
+            this.lb_tienchuyenkhoan.AutoSize = true;
+            this.lb_tienchuyenkhoan.Location = new System.Drawing.Point(226, 227);
+            this.lb_tienchuyenkhoan.Name = "lb_tienchuyenkhoan";
+            this.lb_tienchuyenkhoan.Size = new System.Drawing.Size(44, 15);
+            this.lb_tienchuyenkhoan.TabIndex = 11;
+            this.lb_tienchuyenkhoan.Text = "label13";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(31, 227);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(113, 15);
+            this.label14.TabIndex = 10;
+            this.label14.Text = "Tiền chuyển khoản :";
+            // 
+            // lb_tongdoanhthutrongca
+            // 
+            this.lb_tongdoanhthutrongca.AutoSize = true;
+            this.lb_tongdoanhthutrongca.Location = new System.Drawing.Point(226, 187);
+            this.lb_tongdoanhthutrongca.Name = "lb_tongdoanhthutrongca";
+            this.lb_tongdoanhthutrongca.Size = new System.Drawing.Size(44, 15);
+            this.lb_tongdoanhthutrongca.TabIndex = 9;
+            this.lb_tongdoanhthutrongca.Text = "label15";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(31, 187);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(145, 15);
+            this.label16.TabIndex = 8;
+            this.label16.Text = "Tổng doanh thu trong ca :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(31, 307);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(87, 15);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Tổng tiền mặt :";
+            // 
+            // _tongtienmat
+            // 
+            this._tongtienmat.AutoSize = true;
+            this._tongtienmat.Location = new System.Drawing.Point(226, 307);
+            this._tongtienmat.Name = "_tongtienmat";
+            this._tongtienmat.Size = new System.Drawing.Size(38, 15);
+            this._tongtienmat.TabIndex = 15;
+            this._tongtienmat.Text = "label9";
+            // 
+            // lb_tongdoanhthu
+            // 
+            this.lb_tongdoanhthu.AutoSize = true;
+            this.lb_tongdoanhthu.Location = new System.Drawing.Point(226, 347);
+            this.lb_tongdoanhthu.Name = "lb_tongdoanhthu";
+            this.lb_tongdoanhthu.Size = new System.Drawing.Size(44, 15);
+            this.lb_tongdoanhthu.TabIndex = 17;
+            this.lb_tongdoanhthu.Text = "label17";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(31, 347);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(98, 15);
+            this.label18.TabIndex = 16;
+            this.label18.Text = "Tổng doanh thu :";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(34, 393);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(87, 15);
+            this.label19.TabIndex = 18;
+            this.label19.Text = "Tiền phát sinh :";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(34, 442);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(54, 15);
+            this.label20.TabIndex = 19;
+            this.label20.Text = "Ghi chú :";
+            // 
+            // tbx_tienphatsinh
+            // 
+            this.tbx_tienphatsinh.Location = new System.Drawing.Point(226, 393);
+            this.tbx_tienphatsinh.Name = "tbx_tienphatsinh";
+            this.tbx_tienphatsinh.Size = new System.Drawing.Size(213, 23);
+            this.tbx_tienphatsinh.TabIndex = 20;
+            // 
+            // tbx_ghichu
+            // 
+            this.tbx_ghichu.Location = new System.Drawing.Point(226, 442);
+            this.tbx_ghichu.Name = "tbx_ghichu";
+            this.tbx_ghichu.Size = new System.Drawing.Size(213, 73);
+            this.tbx_ghichu.TabIndex = 21;
+            this.tbx_ghichu.Text = "";
+            // 
+            // btn_resettien
+            // 
+            this.btn_resettien.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btn_resettien.IconColor = System.Drawing.Color.Black;
+            this.btn_resettien.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btn_resettien.Location = new System.Drawing.Point(34, 564);
+            this.btn_resettien.Name = "btn_resettien";
+            this.btn_resettien.Size = new System.Drawing.Size(121, 47);
+            this.btn_resettien.TabIndex = 22;
+            this.btn_resettien.Text = " Rút Tiền";
+            this.btn_resettien.UseVisualStyleBackColor = true;
+            // 
+            // btn_xacnhan
+            // 
+            this.btn_xacnhan.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btn_xacnhan.IconColor = System.Drawing.Color.Black;
+            this.btn_xacnhan.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btn_xacnhan.Location = new System.Drawing.Point(161, 564);
+            this.btn_xacnhan.Name = "btn_xacnhan";
+            this.btn_xacnhan.Size = new System.Drawing.Size(121, 47);
+            this.btn_xacnhan.TabIndex = 23;
+            this.btn_xacnhan.Text = "Xác Nhận";
+            this.btn_xacnhan.UseVisualStyleBackColor = true;
+            // 
+            // btn_huy
+            // 
+            this.btn_huy.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btn_huy.IconColor = System.Drawing.Color.Black;
+            this.btn_huy.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btn_huy.Location = new System.Drawing.Point(288, 564);
+            this.btn_huy.Name = "btn_huy";
+            this.btn_huy.Size = new System.Drawing.Size(121, 47);
+            this.btn_huy.TabIndex = 24;
+            this.btn_huy.Text = "Hủy";
+            this.btn_huy.UseVisualStyleBackColor = true;
+            // 
+            // FrmGiaoCa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 650);
+            this.Controls.Add(this.btn_huy);
+            this.Controls.Add(this.btn_xacnhan);
+            this.Controls.Add(this.btn_resettien);
+            this.Controls.Add(this.tbx_ghichu);
+            this.Controls.Add(this.tbx_tienphatsinh);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.lb_tongdoanhthu);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this._tongtienmat);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lb_tienmat);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lb_tienchuyenkhoan);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.lb_tongdoanhthutrongca);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.lb_tienbandau);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lb_thoigianketthuc);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lb_thoigianbatdau);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lb_maca);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmGiaoCa";
+            this.Text = "FrmGiaoCa";
+            this.Load += new System.EventHandler(this.FrmGiaoCa_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_maca;
+        private System.Windows.Forms.Label lb_thoigianbatdau;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_thoigianketthuc;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lb_tienbandau;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lb_tienmat;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lb_tienchuyenkhoan;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label lb_tongdoanhthutrongca;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label _tongtienmat;
+        private System.Windows.Forms.Label lb_tongdoanhthu;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.TextBox tbx_tienphatsinh;
+        private System.Windows.Forms.RichTextBox tbx_ghichu;
+        private FontAwesome.Sharp.IconButton btn_resettien;
+        private FontAwesome.Sharp.IconButton btn_xacnhan;
+        private FontAwesome.Sharp.IconButton btn_huy;
+    }
+}
---
+++ 3.PL/Views/FrmGiaoCa.cs
@@ -0,0 +1,54 @@
+﻿using _1.DAL.DomainClass;
+using _2.BUS.IServices;
+using _2.BUS.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.PL.Views
+{
+    public partial class FrmGiaoCa : Form
+    {
+
+        public INhanVienService _iNhanVienservice;
+        public IHoaDonChiTietService _iHoaDonchitietservice;
+        public IHoaDonService _ihoadonservice;
+        public IGiaoCaService _iGiaoCaService;
+
+        public FrmGiaoCa()
+        {
+            InitializeComponent();
+            _iGiaoCaService= new GiaoCaService();
+            _ihoadonservice = new HoaDonService();
+            _iHoaDonchitietservice = new HoaDonChiTietService();
+            _iNhanVienservice = new NhanVienService();
+
+        }
+
+        private void FrmGiaoCa_Load(object sender, EventArgs e)
+        {
+            GiaoCa Giaoca = new GiaoCa()
+            {
+                Id = Guid.NewGuid(),
+                Ma = "CA00" + (_iGiaoCaService.GetAllGiaoca().Count + 1),
+                ThoiGianNhanCa = DateTime.Now,
+                TienBatDauCa = 1000000,
+                TrangThai = 0,
+            };
+            _iGiaoCaService.Add(Giaoca);
+            return;
+            foreach (var x in _iGiaoCaService.GetAllGiaoca())
+            {
+                lb_maca.Text = x.Ma;
+                lb_thoigianbatdau.Text = Convert.ToString(x.ThoiGianNhanCa);
+            }
+            
+        }
+    }
+}
