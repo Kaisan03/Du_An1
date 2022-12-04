b203cc076d7c99c392e390eec4f8e41e114c27d6
Hoành <101155961+HoanhDH@users.noreply.github.com>
2022-12-04 16:50:39 +0700

    OKI

---
+++ 2.BUS/Services/GiaoCaService.cs
@@ -0,0 +1,43 @@
+﻿using _1.DAL.DomainClass;
+using _1.DAL.IRepositories;
+using _1.DAL.Repositories;
+using _2.BUS.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.BUS.Services
+{
+    public class GiaoCaService : IGiaoCaService
+    {
+        private IGiaoCaRepository _IGiaoCaRypository;
+        public GiaoCaService()
+        {
+            _IGiaoCaRypository = new GiaoCaRepositpotory();
+        }
+        public bool Add(GiaoCa obj)
+        {
+            _IGiaoCaRypository.Add(obj);
+            return true;
+        }
+
+        public bool Delete(GiaoCa obj)
+        {
+            _IGiaoCaRypository.Delete(obj);
+            return true;
+        }
+
+        public List<GiaoCa> GetAllGiaoca()
+        {
+            return _IGiaoCaRypository.GetAll();
+        }
+
+        public bool Update(GiaoCa obj)
+        {
+            _IGiaoCaRypository.Update(obj);
+            return true;
+        }
+    }
+}
