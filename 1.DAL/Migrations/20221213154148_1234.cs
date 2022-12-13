e2f3e0e7956b89f536898fcb82cb535be2299c8a
Kaisan03 <khainqph24697@fpt.edu.vn>
2022-12-13 22:44:51 +0700

    migration

---
+++ 1.DAL/Migrations/20221213154148_1234.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _1.DAL.Migrations
+{
+    public partial class _1234 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayTraHang",
+                table: "HoaDon",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayThanhToan",
+                table: "HoaDon",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayTao",
+                table: "HoaDon",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayNhanHang",
+                table: "HoaDon",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayGiao",
+                table: "HoaDon",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayTraHang",
+                table: "HoaDon",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayThanhToan",
+                table: "HoaDon",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayTao",
+                table: "HoaDon",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayNhanHang",
+                table: "HoaDon",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayGiao",
+                table: "HoaDon",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+
+           
+        }
+    }
+}
